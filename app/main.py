1a83bea60fb0de0837a1950d727df5dbc22532d3
Bang <Banghalong14@gmail.com>
2026-04-14 21:05:27 +0700

    update main.py

--- app/main.py
+++ app/main.py
@@ -385,3 +385,7 @@ def dai_ly(request: Request):
         name="dai_ly.html",
         context={"current_user": user}
     )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
